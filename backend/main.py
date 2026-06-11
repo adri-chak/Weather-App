b22342795d092c1bb218e3c675aba20957b6fd02
adri-chak <adrija.chakraborty2024@iem.edu.in>
2026-06-11 21:30:27 +0530

    Add dynamic weather lookup using dictionary

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,15 @@ def home():
 def greet(name: str):
     @app.get("/weather")
     def weather(city: str):
+
+        weather_data = {
+            "Kolkata": "32°C",
+            "Delhi": "38°C",
+            "Mumbai": "30°C",
+            "Bangalore": "27°C"
+        }
+
         return {
             "city": city,
-            "temperature": "32°C"
+            "temperature": weather_data.get(city, "Data Not Available")
         }
