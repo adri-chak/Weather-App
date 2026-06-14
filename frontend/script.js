a097bc50a7db54d2addce121c23d903cdb8b81aa
adri-chak <adrija.chakraborty2024@iem.edu.in>
2026-06-14 12:07:34 +0530

    Created JS file

---
+++ frontend/script.js
@@ -0,0 +1,84 @@
+function getWeatherCondition(code) {
+
+            if (code === 0) {
+                return {
+                    icon: "☀️",
+                    text: "Clear Sky"
+                };
+            }
+
+            if ([1, 2, 3].includes(code)) {
+                return {
+                    icon: "☁️",
+                    text: "Cloudy"
+                };
+            }
+
+            if ([45, 48].includes(code)) {
+                return {
+                    icon: "🌫️",
+                    text: "Fog"
+                };
+            }
+
+            if ((code >= 51 && code <= 67) ||
+                (code >= 80 && code <= 82)) {
+                return {
+                    icon: "🌧️",
+                    text: "Rain"
+                };
+            }
+
+            if (code >= 71 && code <= 77) {
+                return {
+                    icon: "❄️",
+                    text: "Snow"
+                };
+            }
+
+            if (code >= 95) {
+                return {
+                    icon: "⛈️",
+                    text: "Thunderstorm"
+                };
+            }
+
+            return {
+                icon: "🌦️",
+                text: `Weather Code ${code}`
+            };
+        }
+
+        async function getWeather() {
+
+            let city = document.getElementById("city").value;
+
+            let response = await fetch(
+                `http://127.0.0.1:8000/weather?city=${city}`
+            );
+
+            let data = await response.json();
+
+            let condition = getWeatherCondition(data.weather_code);
+
+            document.getElementById("result").innerHTML =
+            `
+                <div class="weather-card">
+
+                    <div class="weather-icon">
+                        ${condition.icon}
+                    </div>
+
+                    <h2>${data.city}</h2>
+
+                    <div class="temp">
+                        ${data.temperature}
+                    </div>
+
+                    <p>💨 Wind Speed: ${data.wind_speed}</p>
+
+                    <p>🌤️ ${condition.text}</p>
+
+                </div>
+            `;
+        }
